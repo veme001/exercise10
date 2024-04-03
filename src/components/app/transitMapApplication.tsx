1428bfe168b3fa5f83d4ee616ff2d55288451385
Vegard Merkesvik <veme001@student.kristiania.no>
2024-04-03 15:12:01 +0200

    working example with moving buses and history lines

--- src/components/app/transitMapApplication.tsx
+++ src/components/app/transitMapApplication.tsx
@@ -1,27 +1,30 @@
 import { Map, View } from "ol";
-import { useGeographic } from "ol/proj";
-import React, { MutableRefObject, useEffect, useRef } from "react";
 import TileLayer from "ol/layer/Tile";
 import { OSM } from "ol/source";
+import React, { MutableRefObject, useEffect, useMemo, useRef } from "react";
+import { useGeographic } from "ol/proj";
 
 import "ol/ol.css";
+import { useVehicleLayer } from "./useVehicleLayer";
 
 useGeographic();
 
+const backgroundLayer = new TileLayer({ source: new OSM() });
 const map = new Map({
-  // The map will be centered on 60 degrees latitude and 11 degrees longitude, with a certain zoom level
-  view: new View({ center: [11, 60], zoom: 10 }),
-  // images displayed on the map will be from the Open Street Map (OSM) tile layer
-  layers: [new TileLayer({ source: new OSM() })],
+  view: new View({ center: [10, 63], zoom: 8 }),
 });
 
 export function TransitMapApplication() {
-  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
+  const { vehicleLayer, vehicleTrailLayer } = useVehicleLayer();
+  const layers = useMemo(
+    () => [backgroundLayer, vehicleTrailLayer, vehicleLayer],
+    [vehicleLayer, vehicleLayer],
+  );
+  useEffect(() => map.setLayers(layers), [layers]);
 
+  const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
   useEffect(() => {
     map.setTarget(mapRef.current);
   }, []);
-
-  // This is the location (in React) where we want the map to be displayed
   return <div ref={mapRef}></div>;
 }
---
+++ src/components/app/useVehicles.tsx
@@ -0,0 +1,86 @@
+import { FeedMessage, VehiclePosition } from "../../../generated/gtfs-realtime";
+import { useEffect, useState } from "react";
+
+interface KwsVehiclePosition {
+  coordinate: number[];
+  timestamp: number;
+}
+
+interface KwsVehicle {
+  id: string;
+  routeId: string;
+  position: KwsVehiclePosition;
+  history: KwsVehiclePosition[];
+}
+
+function convertFromProtobuf(
+  vehicle: VehiclePosition | undefined,
+): KwsVehicle | undefined {
+  if (!vehicle) return;
+  const { position, trip, vehicle: protoVehicle } = vehicle;
+  if (!position || !trip || !protoVehicle) return;
+  const { id } = protoVehicle;
+  const { latitude, longitude } = position;
+  const { routeId } = trip;
+  if (!routeId || !id) return;
+
+  const p = {
+    coordinate: [longitude, latitude],
+    timestamp: 0,
+  };
+  return {
+    id,
+    routeId,
+    position: p,
+    history: [p],
+  };
+}
+
+export function useVehicles() {
+  const [vehicleTable, setVehicleTable] = useState<Record<string, KwsVehicle>>(
+    JSON.parse(localStorage.getItem("vehicleTable") || "{}"),
+  );
+  useEffect(() => {
+    localStorage.setItem("vehicleTable", JSON.stringify(vehicleTable));
+  }, [vehicleTable]);
+
+  async function fetchVehiclePosition() {
+    const res = await fetch(
+      "https://api.entur.io/realtime/v1/gtfs-rt/vehicle-positions",
+    );
+    if (!res.ok) {
+      throw `Error fetching ${res.url}: ${res.statusText}`;
+    }
+    const responseMessage = FeedMessage.decode(
+      new Uint8Array(await res.arrayBuffer()),
+    );
+    const vehicles: KwsVehicle[] = [];
+    for (const { vehicle } of responseMessage.entity) {
+      const v = convertFromProtobuf(vehicle);
+      if (v) vehicles.push(v);
+    }
+    setVehicleTable((old) => {
+      const updated = { ...old };
+      for (const v of vehicles) {
+        const oldVehicle = updated[v.id];
+        if (oldVehicle) {
+          updated[v.id] = {
+            ...oldVehicle,
+            position: v.position,
+            history: [...oldVehicle.history, v.position],
+          };
+        } else {
+          updated[v.id] = v;
+        }
+      }
+      return updated;
+    });
+  }
+
+  useEffect(() => {
+    fetchVehiclePosition();
+    const intervalId = setInterval(() => fetchVehiclePosition(), 15000);
+    return () => clearInterval(intervalId);
+  }, []);
+  return Object.values(vehicleTable);
+}
